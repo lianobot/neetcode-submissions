c8c7a12666afb752e19a01363ff7e94119a15e4d
dullah <abdullahrishi30@gmail.com>
2026-09-11 12:33:45 +0500

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.cpp
@@ -0,0 +1,15 @@
+#include <unordered_set>
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+        unordered_set<int> set;
+
+        for (int num : nums){
+            if (set.contains(num)){
+                return true;
+            }
+            set.insert(num);
+        }
+        return false;
+    }
+};
